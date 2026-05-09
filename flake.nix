d8d8f20b7cd5ae5f6fa13b777b1509c176b4c849
Henry Baldwin <henrymbaldwin@proton.me>
2026-05-09 16:15:06 -0400

    build: manage toolchain with flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay.url = "github:oxalica/rust-overlay";
+    };
+
+    outputs = { nixpkgs, flake-utils, rust-overlay, ...}:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                overlays = [ rust-overlay.overlays.default ];
+                pkgs = import nixpkgs { inherit system overlays; };
+
+                rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+            in {
+                devShells.default = pkgs.mkShell {
+                    packages = [
+                        rust
+                        pkgs.cargo-workspaces
+                        pkgs.prek
+                        pkgs.nodejs
+                        pkgs.commitlint
+                    ];
+                };
+            });
+}
